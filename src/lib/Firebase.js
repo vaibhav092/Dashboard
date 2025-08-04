360671b2fb0b2c0dedffaa88687c9273812fcf7d
Vaibhav092 <vaibhavcr7092@gmail.com>
2025-08-05 00:33:49 +0530

    init

--- src/lib/Firebase.js
+++ src/lib/Firebase.js
@@ -3,16 +3,15 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
-    apiKey: 'AIzaSyD3rEexLHqPsrs6xYvRip8Kdg31BgKFBII',
-    authDomain: 'dashboard-cehpoint.firebaseapp.com',
-    projectId: 'dashboard-cehpoint',
-    storageBucket: 'dashboard-cehpoint.firebasestorage.app',
-    messagingSenderId: '1080060874847',
-    appId: '1:1080060874847:web:163cf371137260e55e0bea',
-    measurementId: 'G-F98LZDVWMG',
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
